73d96504c8c23c246988b6498b7ec915f8b76800
Darren Dawson <dwdawson@ucsc.edu>
2019-10-12 20:59:28 -0700

    Add intro code for opening documents that are passed in via args

--- dog.c
+++ dog.c
@@ -5,12 +5,23 @@
 */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 
   // loop over the filenames passed into dog in order to print them
   for (int i = 1; i < argc; i++) {
+
+    int file_pointer = open(argv[i], O_RDONLY);
+    if (file_pointer >= 0) {
+      printf("" + file_pointer);
+      //close(file_pointer);
+    } else {
+      fprintf(stderr, "dog: %s: No such file or directory\n", argv[i]);
+    }
     printf(argv[i]);
+    printf("\n");
   }
 
   return 0;
