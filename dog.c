bfa3b5686ffc31d8c7d6ee30376f4d9fe10adfda
Darren Dawson <dwdawson@ucsc.edu>
2019-10-13 00:20:42 -0700

    Add ability to echo stdout if no files are passed in as arguments

--- dog.c
+++ dog.c
@@ -14,7 +14,7 @@
 // this function takes an opened file pointer
 // it then loads sections of the file into a buffer and prints them
 void print_file(int fd, char *filename) {
-  ssize_t bytes_read = 1;
+  ssize_t bytes_read = 1; // initialize with a positive int
   while (bytes_read > 0) {
     uint8_t *buffer[64]; // this buffer can be any size; we choose 64 here
     bytes_read = read(fd, buffer, sizeof(buffer));
@@ -26,20 +26,38 @@ void print_file(int fd, char *filename) {
   }
 }
 
-// main
+// this function prints out what user is typing into stdin
+void print_stdin() {
+  uint8_t *buffer[8];
+  ssize_t bytes_read = 1; // initialize with a positive int
+  while(bytes_read > 0) {
+    bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+    write(STDOUT_FILENO, buffer, bytes_read);
+  }
+}
+
+
+// if a user specifies files via args, main will run 'cat' functionality on them
+// if no files are specified in args, main will echo stdin
 int main(int argc, char **argv) {
 
-  // loop over the filenames passed into dog in order to print them
-  for (int i = 1; i < argc; i++) {
+  if (argc > 1) {
 
-    int fd = open(argv[i], O_RDONLY);
-    if (fd >= 0) {
-      print_file(fd, argv[i]);
-      close(fd);
-    } else {
-      fprintf(stderr, "dog: %s: No such file or directory\n", argv[i]);
+    // loop over the filenames passed into dog in order to print them
+    for (int i = 1; i < argc; i++) {
+
+      int fd = open(argv[i], O_RDONLY);
+      if (fd >= 0) {
+        print_file(fd, argv[i]);
+        close(fd);
+      } else {
+        fprintf(stderr, "dog: %s: No such file or directory\n", argv[i]);
+      }
     }
+  } else {
+    print_stdin();
   }
+
 
   fprintf(stdout, "\n");
   return 0;
