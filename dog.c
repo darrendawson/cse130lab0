f446895dbdecda002627965a61ca0151797098e9
Darren Dawson <dwdawson@ucsc.edu>
2019-10-12 23:36:24 -0700

    add print_file() function that loads a file piece by piece into a buffer using read(2) and prints to stdout using write(2)

--- dog.c
+++ dog.c
@@ -7,22 +7,35 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+
+// this function takes an opened file pointer
+// it then loads sections of the file into a buffer and prints them
+void print_file(int fd) {
+  int bytes_read = 1;
+  while (bytes_read > 0) {
+    char *buffer[64];
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    write(STDOUT_FILENO, buffer, bytes_read);
+  }
+}
+
+// main
 int main(int argc, char **argv) {
 
   // loop over the filenames passed into dog in order to print them
   for (int i = 1; i < argc; i++) {
 
-    int file_pointer = open(argv[i], O_RDONLY);
-    if (file_pointer >= 0) {
-      printf("" + file_pointer);
-      //close(file_pointer);
+    int fd = open(argv[i], O_RDONLY);
+    if (fd >= 0) {
+      print_file(fd);
+      close(fd);
     } else {
       fprintf(stderr, "dog: %s: No such file or directory\n", argv[i]);
     }
-    printf(argv[i]);
-    printf("\n");
   }
 
+  fprintf(stdout, "\n");
   return 0;
 }
