8463f491d4fa9b624112a78677bf38fe162ac40c
Darren Dawson <dwdawson@ucsc.edu>
2019-10-12 23:53:23 -0700

    Convert (char* -> uint8_t*) and (int->ssize_t) types to adhere to class coding guidelines

--- dog.c
+++ dog.c
@@ -8,16 +8,21 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 
 
 // this function takes an opened file pointer
 // it then loads sections of the file into a buffer and prints them
-void print_file(int fd) {
-  int bytes_read = 1;
+void print_file(int fd, char *filename) {
+  ssize_t bytes_read = 1;
   while (bytes_read > 0) {
-    char *buffer[64];
+    uint8_t *buffer[64]; // this buffer can be any size; we choose 64 here
     bytes_read = read(fd, buffer, sizeof(buffer));
-    write(STDOUT_FILENO, buffer, bytes_read);
+    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written != bytes_read) {
+      fprintf(stderr, "\ndog: %s: error encountered while writing to stdout\n", filename);
+      return;
+    }
   }
 }
 
@@ -29,7 +34,7 @@ int main(int argc, char **argv) {
 
     int fd = open(argv[i], O_RDONLY);
     if (fd >= 0) {
-      print_file(fd);
+      print_file(fd, argv[i]);
       close(fd);
     } else {
       fprintf(stderr, "dog: %s: No such file or directory\n", argv[i]);
